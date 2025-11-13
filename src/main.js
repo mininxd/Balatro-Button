c8673bc43eabf197817ffd602e5566dbd261f6c9
mininxd <mininsna@gmail.com>
2025-11-13 07:10:41 +0000

    User Interface Improvement

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import "./bg.js";
 import "./sprites.js";
 import "./style.css";
-
+import code from "./code.js";
 import Balatro from "balatrobutton";
-// import "/node_modules/balatrobutton/dist/style.css"
+function hex() {
+  const hex = () => Math.floor(Math.random() * 256)
+                      .toString(16)
+                      .padStart(2, "0");
+  return `#${hex()}${hex()}${hex()}`;
+}
+const hex1 = hex();
+const hex2 = hex();
+const hex3 = hex();
+
 const balatroBtn = new Balatro ({
   selector: '.balatroBtn',
   power: () =>  0,
-  colorStart : "#eceeee",
-    colorMid : "#fcd6a9",
-    colorEnd : "#c10cf7",
+  colorStart : hex1,
+    colorMid : hex2,
+    colorEnd : hex3,
 })
 
 
@@ -35,3 +44,41 @@ if(!navigator.userAgent.includes("Mobile")){
   body.classList.add("px-32")
   balatroLogo.classList.add("px-12")
 }
+
+document.querySelectorAll("code").forEach(codeEl => {
+  codeEl.classList.add("whitespace-pre-wrap");
+});
+
+
+quickStartCode.innerHTML = code(`import Balatro from "balatrobutton";
+const element = new Balatro({options});
+
+// Available Options
+{
+  // your element classes
+  selector: '.className',
+  // flame duration
+  duration: 0, // 0 = infinite
+  // flame power
+  power: () => 500,
+  // Initial content
+  score: 0,
+  // Low power
+  colorStart: "#f00",
+  // Medium power
+  colorMid: "#f50",
+  // High power
+  colorEnd: "#ff0",
+};`);
+
+moreCode.innerHTML = code(`// Get fire colors
+console.log(elementBtn.getColors());
+// Change power
+elementBtn.setPower(5000);
+// Change score
+elementBtn.setScore(5000);
+// Stop animation
+elementBtn.stopAll();`)
+  
+cdnCode.innerHTML = code(`import Balatro from "https://unpkg.com/balatrobutton@latest/dist/index.js";
+import "https://unpkg.com/balatrobutton@latest/dist/style.css";`)
